63edce121a707b99b1b8d1a2893be8016d46e058
Joe Schmitz <josephschmitz2018@u.northwestern.edu>
2019-01-26 12:37:56 -0600

    javascript update for list

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,46 +1,91 @@
-// npm install unirest;
-// var unirest = require('unirest');
+// Initialize Firebase
 
-
-
-
-// An example of how the Spoonacular query should look
-// unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=apples%2Cflour%2Csugar")
-// .header("X-RapidAPI-Key", "5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750")
-// .end(function (result) {
-//   console.log(result.status, result.headers, result.body);
-// });
-
-
-var blergh = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=apples%2Cflour%2Csugar";
-
-function callAPI() {
-    
-	var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients??number=5&ranking=1&ingredients=apples%2Cflour%2Csugar&mashape-key=5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750";
-	// var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&limitLicense=false&mashape-key=btSAgzlS6CmshxyNyEh24vDF8sl2p1w43h9jsnCABHsQZSfxx6'
-
-	//Get recipes for specific ingredients from API 
-	var searchByIngredients = new XMLHttpRequest();
-	searchByIngredients.open("GET", queryURL, false);
-	searchByIngredients.send();
-
-	//Parse what API returns (in JSON)
-    var recipes = JSON.parse(searchByIngredients.response);
-    
-    console.log(recipes);
-
-
-
-
-    // $.ajax({
-    //     url: queryURL,
-    //     method: "GET"
-    // }).then(function(response) {
-    //         console.log(response)});
+var config = {
+apiKey: "AIzaSyClhrtpSPee17KydLjp8_LR44ykijOqmR4",
+authDomain: "essential-food-group.firebaseapp.com",
+databaseURL: "https://essential-food-group.firebaseio.com",
+projectId: "essential-food-group",
+storageBucket: "essential-food-group.appspot.com",
+messagingSenderId: "971437057033"
 };
 
-callAPI();
+firebase.initializeApp(config);
 
-// HttpResponse<JsonNode> response = Unirest.get("https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?number=5&ranking=1&ingredients=chicken")
-// .header("X-RapidAPI-Key", "5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750")
-// .asJson();
+// Create variables that refer to the firebase database and the recipe-searches branch
+var database = firebase.database();
+var searches = database.ref("/recipe-searches");
+var results = database.ref("recipe-results");
+
+// on click function to capture what user searches for
+// is there a way for users to search using "Enter" key?
+$("#clickSearch").on("click", function(){
+	var ingredient = $(".search").val().trim();
+	console.log(ingredient);
+	$(".search").val("");
+	// push search term to Firebase database
+	searches.push(ingredient);
+	// redirect to the list.html page
+	location.href = "list.html";
+});
+
+// replicated the landing page search function
+
+$("#listSearch").on("click", function(){
+	var ingredient = $(".listSearch").val().trim();
+	console.log(ingredient);
+	$(".search").val("");
+	// push search term to Firebase database
+	searches.push(ingredient);
+	// redirect to the list.html page
+	location.href = "list.html";
+});
+
+searches.orderByKey().limitToLast(1).on("child_added", function(snapshot) {
+	var ingredient = snapshot.val();
+	
+	console.log("Pulled from Firebase: " + ingredient);
+	var baseURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients??number=5&ranking=1&ingredients="
+	var apiKey = "&mashape-key=5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750";
+	var queryURL = baseURL + ingredient + apiKey;
+	$.ajax({
+		url: queryURL,
+		method: "GET"
+	}).then(function(response) {
+		console.log(response);
+		results.push(response);
+		var i;
+		for (i=0; i < response.length; i++) {
+			var result = $("<div>");
+			var listTitle = response[i].title;
+			var title = $("<p>").text(listTitle);
+			console.log(listTitle);
+			result.attr("id", i);
+			var imgUrl = response[i].image;
+			console.log(imgUrl);
+
+			var listImg = $("<img>");
+			listImg.attr("src", imgUrl);
+
+			$(".recipes").prepend(result);
+			$("#" + i).append(listImg);
+			$("#" + i).append(title)
+		};
+		
+	});
+});
+
+
+// function callAPI() {    
+// 	var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients??number=5&ranking=1&ingredients=apples%2Cflour%2Csugar&mashape-key=5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750";
+// 	// var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&limitLicense=false&mashape-key=btSAgzlS6CmshxyNyEh24vDF8sl2p1w43h9jsnCABHsQZSfxx6'
+
+// 	//Get recipes for specific ingredients from API 
+// 	var searchByIngredients = new XMLHttpRequest();
+// 	searchByIngredients.open("GET", queryURL, false);
+// 	searchByIngredients.send();
+
+// 	//Parse what API returns (in JSON)
+//     var recipes = JSON.parse(searchByIngredients.response);
+    
+//     console.log(recipes);
+// };
