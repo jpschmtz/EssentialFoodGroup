30eb9303ecf7b9e0aa659adc2cda2b1b8700658f
Jessica Chen <jchen@dhcp-10-101-136-235.wireless.northwestern.private>
2019-01-28 18:08:50 -0600

    widget shows

---
+++ assets/javascript/food-widjet.js
@@ -0,0 +1,2 @@
+var spoonacularPriceView = 2;
+
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -52,22 +52,22 @@ searches.on("child_added", function(snapshot) {
 	}).then(function(response) {
 		console.log(response);
 		results.push(response);
-		// console.log(response.id)
+		var i;
+		for (i=0; i < response.length; i++) {
+			var result = $("<div>");
+			var listTitle = response[i].title;
+			var title = $("<p>").text(listTitle);
+			console.log(listTitle);
+			result.attr("id", i);
+			var imgUrl = response[i].image;
+			console.log(imgUrl);
+
+			var listImg = $("<img>");
+			listImg.attr("src", imgUrl);
+
+			$(".recipes").prepend(result);
+			$("#" + i).append(listImg);
+			$("#" + i).append(title)
+		};
 	});
 });
-
-
-// function callAPI() {    
-// 	var queryURL = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients??number=5&ranking=1&ingredients=apples%2Cflour%2Csugar&mashape-key=5b49b5076dmsh8b19b82d3d1e352p188cb3jsn37c3b8570750";
-// 	// var afterIngredients = '&number=200&limitLicense=false&fillIngredients=true&ranking=1&limitLicense=false&mashape-key=btSAgzlS6CmshxyNyEh24vDF8sl2p1w43h9jsnCABHsQZSfxx6'
-
-// 	//Get recipes for specific ingredients from API 
-// 	var searchByIngredients = new XMLHttpRequest();
-// 	searchByIngredients.open("GET", queryURL, false);
-// 	searchByIngredients.send();
-
-// 	//Parse what API returns (in JSON)
-//     var recipes = JSON.parse(searchByIngredients.response);
-    
-//     console.log(recipes);
-// };
