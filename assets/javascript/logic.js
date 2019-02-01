b2bc8d2a0fd3d755a9c63ae629dd6f2cc81ebfe8
speiris <shehanpeiris@gmail.com>
2019-01-31 21:57:21 -0600

    modal for input validation

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -26,13 +26,33 @@ var calories;
 // Define a search function once (globally) that we'll call on click (of search icon) & on enter key-press
 function search() {
 	var ingredient = $(".search").val().trim();
-	console.log(ingredient);
-	console.log("testing global function");
+	if (ingredient === "") {
+		var modal = document.getElementById("myModal");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks on the button, open the modal 
+		modal.style.display = "block";
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		modal.style.display = "none";
+		};
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event) {
+			if (event.target == modal) {
+				modal.style.display = "none";
+			}
+		};
+	} else {
 	$(".search").val("");
 	// push search term to Firebase database
 	searches.push(ingredient);
 	// redirect to the list.html page
 	location.href = "list.html";
+	}
 };
 
 
@@ -72,7 +92,7 @@ searches.orderByKey().limitToLast(1).on("child_added", function (snapshot) {
 		url: queryURL,
 		method: "GET"
 	}).then(function (response) {
-		console.log(response);
+		console.log("RESPONSE: " + response);
 		results.push(response);
 		var i;
 		for (i = 0; i < response.length; i++) {
